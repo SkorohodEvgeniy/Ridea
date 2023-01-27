b578e6aecc83952df2ac8e9eb70357087f9619b4
WebOwl.pro <w.dev@ukr.net>
2023-01-27 18:40:59 +0200

    Add Blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,14 @@ $(document).ready(function () {
     dots: true,
   });
 });
+
+/*----------Slider Blog-----------*/
+$(document).ready(function () {
+  $(".blog__body--lists").slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+  });
+});
