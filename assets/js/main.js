1af4a0a09307eaec886ba9e96388b8d888a8aa4d
Rock-n-Roll <himera80@ukr.net>
2023-01-29 16:25:34 +0200

    Add Page Exchange

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,3 +130,15 @@ function showPopUp() {
 openPopUp.forEach(function (userItem) {
   userItem.addEventListener("click", showPopUp);
 });
+
+
+/*------------ Accordion---------------*/
+$(document).ready(function () {
+  $(".accordion__title").click(function (event) {
+    if ($(".accordion").hasClass("one")) {
+      $(".accordion__title").not($(this)).removeClass("active");
+      $(".accordion__text").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
